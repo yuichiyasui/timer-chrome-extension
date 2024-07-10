ea37d18ae6ebc91e03780f190ee806a1e223454f
yuichiyasui <yassan.b.a.w@gmail.com>
2024-07-11 01:15:53 +0900

    Fix format

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-	plugins: [react()],
-	build: {
-		rollupOptions: {
-			input: {
-				main: "src/main.tsx",
-				background: "src/background.ts",
-			},
-			output: {
-				dir: "dist",
-				format: "es",
-				entryFileNames: "[name].js",
-			},
-		},
-	},
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "src/main.tsx",
+        background: "src/background.ts",
+      },
+      output: {
+        dir: "dist",
+        format: "es",
+        entryFileNames: "[name].js",
+      },
+    },
+  },
 });
