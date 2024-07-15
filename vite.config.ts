de8e7ce7fb86a3e2fc96510fb3390dd40005602d
yuichiyasui <yassan.b.a.w@gmail.com>
2024-07-15 17:40:50 +0900

    Inject css

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		cssInjectedByJsPlugin({
+			jsAssetsFilterFunction: ({ fileName }) => {
+				return fileName.endsWith(".js");
+			},
+		}),
+	],
 	build: {
 		rollupOptions: {
 			input: {
